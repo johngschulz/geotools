f0acc94b8cfabc9524aa050f2c18def8c2ca8ead
John Schulz <schulz.johng@gmail.com>
2018-04-03 14:08:31 -0500

    Implementation of literal expression in MBTypes (#6)

--- modules/unsupported/mbstyle/src/test/java/org/geotools/mbstyle/expression/MBTypeTest.java
+++ modules/unsupported/mbstyle/src/test/java/org/geotools/mbstyle/expression/MBTypeTest.java
@@ -17,13 +17,31 @@
 
 package org.geotools.mbstyle.expression;
 
+<<<<<<< HEAD
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.Test;
+=======
+import org.geotools.mbstyle.MapboxTestUtils;
+import org.geotools.mbstyle.parse.MBObjectParser;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.expression.Expression;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+>>>>>>> Implementation of literal expression in MBTypes (#6)
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+<<<<<<< HEAD
 public class MBTypeTest extends AbstractMBExpressionTest {
 
     @Override
@@ -34,6 +52,24 @@ public class MBTypeTest extends AbstractMBExpressionTest {
     @Override
     protected Class getExpressionClassType() {
         return MBTypes.class;
+=======
+public class MBTypeTest {
+    Map<String, JSONObject> testLayersById = new HashMap<>();
+    MBObjectParser parse;
+    FilterFactory2 ff;
+    JSONObject mbstyle;
+
+    @Before
+    public void setUp() throws IOException, ParseException {
+        mbstyle = MapboxTestUtils.parseTestStyle("expressionParseTypesTest.json");
+        JSONArray layers = (JSONArray) mbstyle.get("layers");
+        for (Object o : layers) {
+            JSONObject layer = (JSONObject) o;
+            testLayersById.put((String) layer.get("id"), layer);
+        }
+        parse = new MBObjectParser(MBExpression.class);
+        ff = parse.getFilterFactory();
+>>>>>>> Implementation of literal expression in MBTypes (#6)
     }
 
     /**
@@ -42,21 +78,65 @@ public class MBTypeTest extends AbstractMBExpressionTest {
     @Test
     public void testParseLiteralExpression() {
 
+<<<<<<< HEAD
         final JSONObject j = getObjectByLayerId("literalExpression", "paint");
 
         // test for literal JSONArray
         Object array = getExpressionEvaluation(j, "circle-radius");
+=======
+        JSONObject layer = testLayersById.get("literalExpression");
+        Optional<JSONObject> o = traverse(layer, JSONObject.class, "paint");
+        JSONObject j = o.get();
+
+        // test for literal JSONArray
+        assertEquals(JSONArray.class, j.get("circle-radius").getClass());
+        JSONArray arr0 = (JSONArray) j.get("circle-radius");
+        assertEquals(MBTypes.class, MBExpression.create(arr0).getClass());
+        Expression litArr = MBExpression.transformExpression(arr0);
+        Object array = litArr.evaluate(litArr);
+>>>>>>> Implementation of literal expression in MBTypes (#6)
         assertTrue(array instanceof JSONArray);
         JSONArray jarray = (JSONArray)array;
         assertEquals(4, jarray.size());
         assertEquals("string", (jarray.get(1)));
 
         // test for literal JSONObject
+<<<<<<< HEAD
         Object obj = getExpressionEvaluation(j, "circle-color");
+=======
+        assertEquals(JSONArray.class, j.get("circle-color").getClass());
+        JSONArray arr1 = (JSONArray) j.get("circle-color");
+        assertEquals(MBTypes.class, MBExpression.create(arr1).getClass());
+        Expression litObj = MBExpression.transformExpression(arr1);
+        Object obj = litObj.evaluate(litObj);
+>>>>>>> Implementation of literal expression in MBTypes (#6)
         assertTrue(obj instanceof JSONObject);
         JSONObject jobj = (JSONObject) obj;
         assertEquals(2, jobj.size());
         assertEquals(jobj.get("object"), "test");
 
     }
+<<<<<<< HEAD
+=======
+
+    private <T> Optional<T> traverse(JSONObject map, Class<T> clazz,
+                                     String... path) {
+        if (path == null || path.length == 0) {
+            return Optional.empty();
+        }
+
+        Object value = map;
+        for (String key : path) {
+            if (value instanceof JSONObject) {
+                JSONObject m = (JSONObject) value;
+                if (m.containsKey(key)) {
+                    value = m.get(key);
+                }
+            } else {
+                return Optional.empty();
+            }
+        }
+        return Optional.of(clazz.cast(value));
+    }
+>>>>>>> Implementation of literal expression in MBTypes (#6)
 }
