3499c0bce7ca874719243b789a0e462c0cc94851
johngschulz <schulz.johng@gmail.com>
2018-04-05 10:47:14 -0500

    refactoring test

--- modules/unsupported/mbstyle/src/test/java/org/geotools/mbstyle/expression/MBZoomTest.java
+++ modules/unsupported/mbstyle/src/test/java/org/geotools/mbstyle/expression/MBZoomTest.java
@@ -18,59 +18,25 @@ import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
-public class MBZoomTest {
-    Map<String, JSONObject> testLayersById = new HashMap<>();
-    MBObjectParser parse;
-    FilterFactory2 ff;
-    JSONObject mbstyle;
+public class MBZoomTest extends AbstractMBExpressionTest {
+    @Override
+    protected String getTestResourceName() {
+        return "expressionMBZoomTest.json";
+    }
 
-    @Before
-    public void setUp() throws IOException, ParseException {
-        mbstyle = MapboxTestUtils.parseTestStyle("expressionParseTestZoom.json");
-        JSONArray layers = (JSONArray) mbstyle.get("layers");
-        for (Object o : layers) {
-            JSONObject layer = (JSONObject) o;
-            testLayersById.put((String) layer.get("id"), layer);
-        }
-        parse = new MBObjectParser(MBExpression.class);
-        ff = parse.getFilterFactory();
+    @Override
+    protected Class getExpressionClassType() {
+        return MBZoom.class;
     }
 
     /**
-     * Verify that a upcase string expression can be parsed correctly.
+     * Verify that the "zoom" MBTypes expression can be parsed correctly.
      */
     @Test
-    public void testParseZoomExpression() {
-
-        JSONObject layer = testLayersById.get("zoomExpression");
-        Optional<JSONObject> o = traverse(layer, JSONObject.class, "paint");
-        JSONObject j = o.get();
-        assertEquals(JSONArray.class, j.get("circle-radius").getClass());
-        JSONArray arr = (JSONArray) j.get("circle-radius");
+    public void testParseLiteralExpression() {
         EnvFunction.setGlobalValue("wms_scale_denominator", "2132.729584");
-        assertEquals(MBZoom.class, MBExpression.create(arr).getClass());
-        Expression zoom = MBExpression.transformExpression(arr);
-        Object level = zoom.evaluate(zoom);
-        assertEquals(ff.literal(Math.round(17.999)), ff.literal(Math.round((double)level)));
-    }
-
-    private <T> Optional<T> traverse(JSONObject map, Class<T> clazz,
-                                     String... path) {
-        if (path == null || path.length == 0) {
-            return Optional.empty();
-        }
-
-        Object value = map;
-        for (String key : path) {
-            if (value instanceof JSONObject) {
-                JSONObject m = (JSONObject) value;
-                if (m.containsKey(key)) {
-                    value = m.get(key);
-                }
-            } else {
-                return Optional.empty();
-            }
-        }
-        return Optional.of(clazz.cast(value));
+        final JSONObject j = getObjectByLayerId("zoomExpression", "paint");
+        Object o = getExpressionEvaluation(j, "circle-radius");
+        assertEquals(ff.literal(Math.round(17.999)), ff.literal(Math.round((double) o)));
     }
 }
